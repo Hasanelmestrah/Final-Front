7e80cb02ea7219401a47b5806ac670f51a38c62e
hasanelmestrah <hasanelmestrah@gmail.com>
2023-06-05 12:46:38 +0300

    last days

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -3,8 +3,8 @@ import "./loader.css";
 
 const Loader = () => {
   return (
-    <div class="loader">
-      <div class="justify-content-center jimu-primary-loading"></div>
+    <div className="loader">
+      <div className="justify-content-center jimu-primary-loading"></div>
     </div>
   );
 };
--- src/Pages/Aboutus/AboutUs.jsx
+++ src/Pages/Aboutus/AboutUs.jsx
@@ -8,23 +8,17 @@ function AboutUs() {
         <div className="text">
           <h2>About Us</h2>
           <h5>
-            buy your perfect product &amp;{" "}
-            <span className="aboutspan">Trust</span>
+            Buy Your Quality Product &amp;{" "}
+            <span className="aboutspan">More</span>
           </h5>
           <p>
-            Welcome to our e-commerce website, your one-stop destination for a
-            wide range of quality products! We take immense pride in offering a
-            diverse selection of items that cater to your needs and desires.
-            Whether you're searching for fashion apparel, electronic gadgets,
-            home decor, or personal accessories, we've got you covered. Our
-            commitment to excellence ensures that every product on our website
-            meets the highest standards of quality. We understand the importance
-            of convenience, which is why we provide the option of cash on
-            delivery, allowing you to shop with ease and confidence. Discover a
-            seamless shopping experience where you can explore a variety of
-            products, experience superior quality, and enjoy the convenience of
-            cash on delivery. Start browsing now and find the perfect items to
-            enhance your lifestyle.
+            Welcome to your Home, the ultimate destination for a wide range of
+            high-quality products. Explore our diverse selection of electronics
+            gadgets, solar power, and personal accessories. Rest assured that
+            every product on our website meets the highest standards of
+            excellence. Enjoy the convenience of cash on delivery, making your
+            shopping experience effortless and worry-free. Start browsing now to
+            discover superior quality items that will enhance your lifestyle.
           </p>
           <div className="data">
             <a href="/contactus" className="aboutbutton">
